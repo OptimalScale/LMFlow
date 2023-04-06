55a4bd938a1a465bd9e3ce024f4c470a627d8554
diaoshizhe <654745845@qq.com>
2023-04-06 14:23:24 +0800

    developing vicuna

--- scripts/run_chatbot.sh
+++ scripts/run_chatbot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-model=gpt2
+model=eachadea/vicuna-13b
 lora_args=""
 if [ $# -ge 1 ]; then
   model=$1
@@ -9,8 +9,9 @@ if [ $# -ge 2 ]; then
   lora_args="--lora_model_path $2"
 fi
 
-CUDA_VISIBLE_DEVICES=0 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
   deepspeed examples/chatbot.py \
+      --use_ram_optimized_load False \
       --deepspeed configs/ds_config_chatbot.json \
       --model_name_or_path ${model} \
       ${lora_args}
