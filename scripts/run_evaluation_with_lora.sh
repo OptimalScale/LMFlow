7c7613448eaebfaa6517342a8b1459d0dd5829e0
yaoguany <89233842+yaoguany@users.noreply.github.com>
2023-04-07 09:29:00 +0800

    batch inference in evaluator.py

--- scripts/run_evaluation_with_lora.sh
+++ scripts/run_evaluation_with_lora.sh
@@ -3,15 +3,13 @@
 # --model_name_or_path specifies the original huggingface model
 # --lora_model_path specifies the model difference introduced by finetuning,
 #   i.e. the one saved by ./scripts/run_finetune_with_lora.sh
-#for input in "Consider it carefully:" "give correct answer:" "Concise""responsible"
-CUDA_VISIBLE_DEVICES=1 \
+CUDA_VISIBLE_DEVICES=0 \
     deepspeed examples/evaluate.py \
     --answer_type usmle \
     --model_name_or_path pinkmanlove/llama-7b-hf \
     --lora_model_path output_models/llama7b-lora-medical \
     --dataset_path data/MedQA-USMLE/validation \
-    --prompt_structure "Good job confident accurate:{input}" \
+    --prompt_structure "Input: {input}" \
     --batch_size 30 \
-    --deepspeed examples/ds_config.json \
-#Which answer is the best?Please consider it carefully:
-#confident
+    --deepspeed examples/ds_config.json
+#!/bin/bash
