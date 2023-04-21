248c15af332e183b17c712774a14f360709f7b1d
Hanze Dong <18676401+hendrydong@users.noreply.github.com>
2023-04-21 15:47:14 +0800

    update

--- examples/chatbot_gradio.py
+++ examples/chatbot_gradio.py
@@ -1,95 +1,36 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+#!/usr/bin/env python
+# coding=utf-8
+# Copyright 2023 Statistics and Machine Learning Research Group at HKUST. All rights reserved.
+"""A simple shell chatbot implemented with lmflow APIs.
+"""
+import logging
+import json
+import sys
+import warnings
 import gradio as gr
+from dataclasses import dataclass, field
+from transformers import HfArgumentParser
+from typing import Optional
 
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
-model = AutoModelForCausalLM.from_pretrained("gpt2").half().cuda()
-model = model.eval()
+from lmflow.datasets.dataset import Dataset
+from lmflow.pipeline.auto_pipeline import AutoPipeline
+from lmflow.models.auto_model import AutoModel
+from lmflow.args import ModelArguments, DatasetArguments, AutoArguments
 
-MAX_TURNS = 20
-MAX_BOXES = MAX_TURNS * 2
+MAX_BOXES = 20
 
-MAX_INPUT_LEN = 512
-
-MAX_LENGTH = 512
-
-def generate_stream(model, tokenizer, delta_tokens, max_length, **kwargs):
-
-    base_p = len(kwargs["input_ids"][0])
-    last_p = base_p
-    if len(kwargs["input_ids"][0])>MAX_INPUT_LEN:
-        kwargs["input_ids"][0] = kwargs["input_ids"][0][-MAX_INPUT_LEN:]
-    kwargs["max_length"] = len(kwargs["input_ids"][0]) + delta_tokens
-    while True:
-
-        output_ids = model.generate(kwargs["input_ids"],
-                        max_length=kwargs["max_length"],
-                        do_sample = kwargs["do_sample"],
-                        top_p = kwargs["top_p"],
-                        temperature=kwargs["temperature"],
-                        repetition_penalty=1.0)
-        
-        output_seq = output_ids[0].tolist()
-
-        eos_p = len(output_seq)
-
-        return_delta = output_seq[last_p: eos_p]
-        return_seq = output_seq[base_p: eos_p]
-        yield return_delta, return_seq
-
-        if kwargs["max_length"] >= max_length:
-            break
-
-        kwargs["input_ids"] = output_ids
-        kwargs["max_length"] = len(output_ids[0]) + delta_tokens
-        last_p = eos_p
-
-
-def chat_stream(model, tokenizer, query: str, history= None, delta_tokens= 2, max_length= 256, do_sample=True, top_p=0.7, temperature=0.95, logits_processor=None, **kwargs):
-    if history is None:
-        history = []
-
-    gen_kwargs = {"do_sample": do_sample, "top_p": top_p, "temperature": temperature,**kwargs}
-    if not history:
-        prompt = query
-    else:
-        prompt = ""
-        for i, (old_query, response) in enumerate(history):
-            prompt += "### Human:{} ### Assistent:{}".format( old_query, response)
-        prompt += "### Human:{} ### Assistent:".format(query)
-    input_ids = tokenizer([prompt], return_tensors="pt")
-    input_ids = input_ids.to(model.device)
-    for delta, seq in generate_stream(model,tokenizer,delta_tokens, max_length, **input_ids, **gen_kwargs):
-        if delta:
-            delta = tokenizer.decode(delta)
-        else:
-            delta = ""
-        if seq:
-            seq = tokenizer.decode(seq)
-        else:
-            seq = ""
-        yield delta, history + [(query, seq)]
-
-
-
-
-def predict(input, max_length, top_p, temperature, history=None):
-    if history is None:
-        history = []
-    for response, history in chat_stream(model,tokenizer, input, history, max_length=max_length*(len(history)+1), top_p=top_p,
-                                               temperature=temperature):
-        updates = []
-        for query, response in history:
-            updates.append(gr.update(visible=True, value="" + query))
-            updates.append(gr.update(visible=True, value="" + response))
-        if len(updates) < MAX_BOXES:
-            updates = updates + [gr.Textbox.update(visible=False)] * (MAX_BOXES - len(updates))
-        yield [history] + updates
+logging.disable(logging.ERROR)
+warnings.filterwarnings("ignore")
 
 title = """
 <h1 align="center">LMFlow-CHAT</h1>
 <link rel="stylesheet" href="/path/to/styles/default.min.css">
 <script src="/path/to/highlight.min.js"></script>
 <script>hljs.highlightAll();</script>
+
+<p>LMFlow is in extensible, convenient, and efficient toolbox for finetuning large machine learning models, designed to be user-friendly, speedy and reliable, and accessible to the entire community.</p>
+
+<p>We have thoroughly tested this toolkit and are pleased to make it available under <a class="reference external" href="https://github.com/OptimalScale/LMFlow">Github</a>.</p>
 """
 css = """
 #user {                                                                         
@@ -102,7 +43,7 @@ css = """
     line-height: 32px;
     padding: 2px 8px;
     font-size: 14px;
-    background:	#113285;
+    background:	#9DC284;
     border-radius:5px; 
     margin:10px 0px;
 }
@@ -117,33 +58,166 @@ css = """
     line-height: 32px;
     padding: 2px 8px;
     font-size: 14px;
-    background:#0C4842;
+    background:#7BA7D7;
     border-radius:5px; 
     margin:10px 0px;
 }
 """
-with gr.Blocks(css=css) as demo:
-    gr.HTML(title)
-    state = gr.State([])
-    text_boxes = []
-    for i in range(MAX_BOXES):
-        if i % 2 == 0:
-            text_boxes.append(gr.Markdown(visible=False, label="Q:", elem_id="user"))
-        else:
-            text_boxes.append(gr.Markdown(visible=False, label="A:", elem_id="chatbot"))
 
-    with gr.Column(elem_id = "col_container"):
-        with gr.Row():
-            with gr.Column(scale=19):
-                txt = gr.Textbox(
-                    show_label=False,
-                    placeholder="Enter text and press enter",
-                ).style(container=True)
-            with gr.Column(scale=1):
-                button = gr.Button("Send")
-        with gr.Accordion("Parameters", open=False):
-            max_length = gr.Slider(0, MAX_LENGTH, value=128, step=1.0, label="Maximum length", interactive=True)
-            top_p = gr.Slider(0, 1, value=0.7, step=0.01, label="Top P", interactive=True)
-            temperature = gr.Slider(0, 1, value=0.7, step=0.01, label="Temperature", interactive=True)
-    button.click(predict, [txt, max_length, top_p, temperature, state], [state] + text_boxes)
-demo.queue().launch(share=True)
+
+@dataclass
+class ChatbotArguments:
+    prompt_structure: Optional[str] = field(
+        default="{input_text}",
+        metadata={
+            "help": "prompt structure given user's input text"
+        },
+    )
+    end_string: Optional[str] = field(
+        default="\n\n",
+        metadata={
+            "help": "end string mark of the chatbot's output"
+        },
+    )
+    max_new_tokens: Optional[int] = field(
+        default=200,
+        metadata={
+            "help": "maximum number of generated tokens"
+        },
+    )
+    temperature: Optional[float] = field(
+        default=0.7,
+        metadata={
+            "help": "higher this value, more random the model output"
+        },
+    )
+
+
+def main():
+    pipeline_name = "inferencer"
+    PipelineArguments = AutoArguments.get_pipeline_args_class(pipeline_name)
+
+    parser = HfArgumentParser((
+        ModelArguments,
+        PipelineArguments,
+        ChatbotArguments,
+    ))
+    model_args, pipeline_args, chatbot_args = (
+        parser.parse_args_into_dataclasses()
+    )
+
+    with open (pipeline_args.deepspeed, "r") as f:
+        ds_config = json.load(f)
+
+    model = AutoModel.get_model(
+        model_args,
+        tune_strategy='none',
+        ds_config=ds_config,
+        device=pipeline_args.device,
+    )
+
+    # We don't need input data, we will read interactively from stdin
+    data_args = DatasetArguments(dataset_path=None)
+    dataset = Dataset(data_args)
+
+    inferencer = AutoPipeline.get_pipeline(
+        pipeline_name=pipeline_name,
+        model_args=model_args,
+        data_args=data_args,
+        pipeline_args=pipeline_args,
+    )
+
+    # Chats
+    model_name = model_args.model_name_or_path
+    if model_args.lora_model_path is not None:
+        model_name += f" + {model_args.lora_model_path}"
+
+
+    # context = (
+    #     "You are a helpful assistant who follows the given instructions"
+    #     " unconditionally."
+    # )
+
+
+    end_string = chatbot_args.end_string
+    prompt_structure = chatbot_args.prompt_structure
+
+
+    token_per_step = 4
+
+
+    def chat_stream( context, query: str, history= None, **kwargs):
+        if history is None:
+            history = []
+
+        print_index = 0
+        context += prompt_structure.format(input_text=query)
+        context = context[-model.get_max_length():] 
+        input_dataset = dataset.from_dict({
+            "type": "text_only",
+            "instances": [ { "text": context } ]
+        })
+        for response, flag_break in inferencer.stream_inference(context=context, model=model, max_new_tokens=chatbot_args.max_new_tokens, 
+                                        token_per_step=token_per_step, temperature=chatbot_args.temperature,
+                                        end_string=end_string, input_dataset=input_dataset):
+            delta = response[print_index:]
+            seq = response
+            print_index = len(response)
+            
+            yield delta, history + [(query, seq)]
+            if flag_break:
+                context += response + "\n"
+                break
+
+
+
+
+    def predict(input, history=None): 
+        try:
+            global context
+            context = ""
+        except SyntaxError:
+            pass
+
+        if history is None:
+            history = []
+        for response, history in chat_stream(context, input, history):
+            updates = []
+            for query, response in history:
+                updates.append(gr.update(visible=True, value="" + query))
+                updates.append(gr.update(visible=True, value="" + response))
+            if len(updates) < MAX_BOXES:
+                updates = updates + [gr.Textbox.update(visible=False)] * (MAX_BOXES - len(updates))
+            yield [history] + updates
+
+
+
+
+
+    with gr.Blocks(css=css) as demo:
+        gr.HTML(title)
+        state = gr.State([])
+        text_boxes = []
+        for i in range(MAX_BOXES):
+            if i % 2 == 0:
+                text_boxes.append(gr.Markdown(visible=False, label="Q:", elem_id="user"))
+            else:
+                text_boxes.append(gr.Markdown(visible=False, label="A:", elem_id="chatbot"))
+
+        with gr.Column(elem_id = "col_container"):
+            with gr.Row():
+                with gr.Column(scale=19):
+                    txt = gr.Textbox(
+                        show_label=False,
+                        placeholder="Enter text and press send",
+                    )
+                with gr.Column(scale=1):
+                    button = gr.Button("Send")
+        button.click(predict, [txt, state], [state] + text_boxes)
+        demo.queue().launch(share=True)
+
+
+
+
+if __name__ == "__main__":
+    main()
