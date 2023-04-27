e6d3449f653bdd6091ab4b57a7416dc83e878a7e
Domnic789654 <xliu29@gmu.edu>
2023-04-27 20:48:38 +0800

    test eval

--- finetune_xl.sh
+++ finetune_xl.sh
@@ -1,4 +1,4 @@
-wandb online
+wandb offline
 lr=8e-4
 bs=4
 use_lora=1
@@ -7,11 +7,11 @@ gradient_checkpointing=False
 gradient_accumulation_steps=4
 lora_r=32
 ds_config=configs/ds_config_zero2.json
-model_name_or_path=pinkmanlove/llama-7b-hf
-exp_name="xl_058";
+model_name_or_path=gpt2
+exp_name="test";
 data_path="/home/xiangliu/LMFlow/data/cn_v1_HA"
 # data_path="/home/xiangliu/LMFlow/data/example_dataset/train"
 eval_dataset_path="/home/xiangliu/LMFlow/data/gpt4_eval"
-bash ./scripts/run_finetune_with_lora_save_aggregated_weights.sh ${exp_name} ${data_path} ${lr} ${bs} ${model_name_or_path} ${use_lora} ${ds_config} ${epochs} ${gradient_checkpointing} ${gradient_accumulation_steps} ${lora_r} ${eval_dataset_path} "--master_port=10065 --num_gpus=8" 
+bash ./scripts/run_finetune_with_lora_save_aggregated_weights.sh ${exp_name} ${data_path} ${lr} ${bs} ${model_name_or_path} ${use_lora} ${ds_config} ${epochs} ${gradient_checkpointing} ${gradient_accumulation_steps} ${lora_r} ${eval_dataset_path} "--master_port=10065 --num_gpus=1" 
 
 # bash ./scripts/run_chatbot_vicuna_test_HA.sh ./output_models/${exp_name} > ./chatbot_logs/${exp_name}.log 2>&1 
