392bcf13afdc5e7b694802f123b30c2a012f628e
Domnic789654 <xliu29@gmu.edu>
2023-04-27 21:00:46 +0800

    eval pass

--- finetune_xl.sh
+++ finetune_xl.sh
@@ -1,7 +1,7 @@
 wandb offline
 lr=8e-4
 bs=4
-use_lora=1
+use_lora=0
 epochs=10
 gradient_checkpointing=False
 gradient_accumulation_steps=4
@@ -9,9 +9,8 @@ lora_r=32
 ds_config=configs/ds_config_zero2.json
 model_name_or_path=gpt2
 exp_name="test";
-data_path="/home/xiangliu/LMFlow/data/cn_v1_HA"
-# data_path="/home/xiangliu/LMFlow/data/example_dataset/train"
+# data_path="/home/xiangliu/LMFlow/data/cn_v1_HA"
+data_path="/home/xiangliu/LMFlow/data/example_dataset/train"
 eval_dataset_path="/home/xiangliu/LMFlow/data/gpt4_eval"
 bash ./scripts/run_finetune_with_lora_save_aggregated_weights.sh ${exp_name} ${data_path} ${lr} ${bs} ${model_name_or_path} ${use_lora} ${ds_config} ${epochs} ${gradient_checkpointing} ${gradient_accumulation_steps} ${lora_r} ${eval_dataset_path} "--master_port=10065 --num_gpus=1" 
 
-# bash ./scripts/run_chatbot_vicuna_test_HA.sh ./output_models/${exp_name} > ./chatbot_logs/${exp_name}.log 2>&1 
