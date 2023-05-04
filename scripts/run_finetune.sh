2c0b1cd8d92d6e16a7e08ba7de5842ff848e7319
2003pro <zhangjipeng20@outlook.com>
2023-05-05 02:29:03 +0800

    Remove unnecessary modification on run_finetune.sh

--- scripts/run_finetune.sh
+++ scripts/run_finetune.sh
@@ -2,7 +2,6 @@
 # Please run this script under ${project_id} in project directory of
 #   https://github.com/shizhediao/llm-ft
 #     COMMIT: d5fecf30ba8011067b10cf51fede53a5ab6574e4
-export WANDB_MODE=disabled
 
 deepspeed_args="--master_port=11000"      # Default argument
 if [ $# -ge 1 ]; then
@@ -20,14 +19,13 @@ mkdir -p ${output_dir} ${log_dir}
 
 deepspeed ${deepspeed_args} \
   examples/finetune.py \
-    --model_name_or_path  pinkmanlove/llama-7b-hf  \
-    --use_ram_optimized_load 0 \
+    --model_name_or_path gpt2 \
     --dataset_path ${dataset_path} \
     --output_dir ${output_dir} --overwrite_output_dir \
-    --num_train_epochs 1 \
+    --num_train_epochs 0.01 \
     --learning_rate 2e-5 \
-    --block_size 2048 \
-    --per_device_train_batch_size 24 \
+    --block_size 512 \
+    --per_device_train_batch_size 1 \
     --deepspeed configs/ds_config_zero3.json \
     --bf16 \
     --run_name finetune \
@@ -37,7 +35,5 @@ deepspeed ${deepspeed_args} \
     --ddp_timeout 72000 \
     --save_steps 5000 \
     --dataloader_num_workers 1 \
-    --use_flash_attention 1\
-     --gradient_checkpointing True \
     | tee ${log_dir}/train.log \
     2> ${log_dir}/train.err
