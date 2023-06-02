266bf969cb83f48c37170d97a06c11fd4d62f941
SHUMKASHUN <ksshumab@connect.ust.hk>
2023-06-02 16:08:17 +0800

    lmflow-finetune 0.0.3

--- src/lmflow/args.py
+++ src/lmflow/args.py
@@ -702,24 +702,6 @@ class RaftAlignerArguments(TrainingArguments):
         },
     )
 
-@dataclass
-class BenchmarkingArguments:
-    dataset_name: Optional[str] = field(
-        default=None,
-        metadata={
-            "help": "benchmark dataset name provided by lmflow"
-        },
-    )
-    lm_evaluation_metric: Optional[str] = field(
-        default="accuracy",
-        metadata={
-            "help": "the metric the model will be evaluated on",
-            "choices": ["acc", "acc_norm", "bleu", "chrf", "em", "f1", "ppl", \
-                "ter", "r@1", "r@2", "mrr", "mc1", "mc2", "word_perplexity", \
-                    "byte_perplexity", "bits_per_byte"],
-        },
-    )
-
 PIPELINE_ARGUMENT_MAPPING = {
     "finetuner": FinetunerArguments,
     "evaluator": EvaluatorArguments,
