c3f09e68f044c9c464ac7f51d9147ac800dad8df
sYc <songyc9@126.com>
2023-06-08 14:40:24 +0800

    deleted the part of test.py

--- src/lmflow/pipeline/test_rougel.py
+++ src/lmflow/pipeline/test_rougel.py
@@ -12,7 +12,7 @@ from functools import partial
 # TODO: remove later
 from transformers import AutoConfig
 # from lmflow.pipeline.auto_pipeline import AutoPipeline
-import evaluate
+
 import torch.distributed as dist
 from transformers import HfArgumentParser
 from lmflow.datasets.dataset import Dataset
@@ -20,29 +20,6 @@ from lmflow.pipeline.base_pipeline import BasePipeline
 from lmflow.utils.data_utils import set_random_seed, batchlize, answer_extraction, load_data
 from lmflow.models.auto_model import AutoModel
 from lmflow.args import ModelArguments, DatasetArguments, AutoArguments
-
-os.environ["TOKENIZERS_PARALLELISM"] = "false"  # To avoid warnings about parallelism in tokenizers
-
-# copied from evaluate.py, with small changes.
-pipeline_name = "test_rougel"
-PipelineArguments = AutoArguments.get_pipeline_args_class(pipeline_name)
-
-parser = HfArgumentParser((ModelArguments, DatasetArguments, PipelineArguments))
-model_args, data_args, pipeline_args = parser.parse_args_into_dataclasses()
-
-with open (pipeline_args.deepspeed, "r") as f:
-    ds_config = json.load(f)
-
-model = AutoModel.get_model(model_args, tune_strategy='none', ds_config=ds_config)
-dataset = Dataset(data_args)
-
-evaluator = AutoPipeline.get_pipeline(
-    pipeline_name=pipeline_name,
-    model_args=model_args,
-    data_args=data_args,
-    pipeline_args=pipeline_args,
-)
-evaluator.evaluate(model=model, dataset=dataset, metric=pipeline_args.metric)
 
 #copied from evaluator.py
 class Test_rougel(BasePipeline):
@@ -301,6 +278,5 @@ class Test_rougel(BasePipeline):
                 print("scores not matched!")
                 return
         print("scores matched. Tested to be valid.")
-
 
 
