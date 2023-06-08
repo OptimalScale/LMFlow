91e61cf091226fa3661f5a3131cbcfa66b77893c
sYc <songyc9@126.com>
2023-06-08 15:08:33 +0800

    copied the function of Auto_pipeline inside

--- examples/test.py
+++ examples/test.py
@@ -11,7 +11,8 @@ from multiprocessing import Pool
 from functools import partial
 # TODO: remove later
 from transformers import AutoConfig
-from lmflow.pipeline.auto_pipeline import AutoPipeline
+
+# from lmflow.pipeline.auto_pipeline import AutoPipeline
 
 import torch.distributed as dist
 from transformers import HfArgumentParser
@@ -20,8 +21,28 @@ from lmflow.pipeline.base_pipeline import BasePipeline
 from lmflow.utils.data_utils import set_random_seed, batchlize, answer_extraction, load_data
 from lmflow.models.auto_model import AutoModel
 from lmflow.args import ModelArguments, DatasetArguments, AutoArguments
+from lmflow.pipeline.test_rougel import Test_rougel
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"  # To avoid warnings about parallelism in tokenizers
+
+
+def get_pipeline(self,
+                 pipeline_name,
+                 model_args,
+                 data_args,
+                 pipeline_args,
+                 *args,
+                 **kwargs
+                 ):
+
+    pipeline = Test_rougel(
+        model_args,
+        data_args,
+        pipeline_args,
+        *args,
+        **kwargs
+    )
+    return pipeline
 
 # copied from evaluate.py, with small changes.
 pipeline_name = "test_rougel"
@@ -36,7 +57,7 @@ with open (pipeline_args.deepspeed, "r") as f:
 model = AutoModel.get_model(model_args, tune_strategy='none', ds_config=ds_config)
 dataset = Dataset(data_args)
 
-evaluator = AutoPipeline.get_pipeline(
+evaluator = get_pipeline(
     pipeline_name=pipeline_name,
     model_args=model_args,
     data_args=data_args,
