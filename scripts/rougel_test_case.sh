f46e99b9df74705cbdaca02264b1239f0c23d465
sYc <songyc9@126.com>
2023-06-08 14:39:43 +0800

    split test.py and test_rougel.py

--- scripts/rougel_test_case.sh
+++ scripts/rougel_test_case.sh
@@ -3,7 +3,7 @@
 # 比较self-instruct和LMFlow对相同instructions的ROUGE-L得分是否相同
 
 CUDA_VISIBLE_DEVICES=0 \
-    deepspeed examples/test_rougel.py \
+    deepspeed examples/test.py \
     --answer_type text \
     --model_name_or_path gpt2-large \
     --dataset_path data/alpaca/test \
