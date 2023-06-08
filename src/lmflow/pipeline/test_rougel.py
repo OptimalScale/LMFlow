f74cb7585709081965cc8703fd7d3f94226dbd8c
sYc <songyc9@126.com>
2023-06-08 14:19:36 +0800

    Add files via upload

--- src/lmflow/pipeline/test_rougel.py
+++ src/lmflow/pipeline/test_rougel.py
@@ -11,17 +11,41 @@ from multiprocessing import Pool
 from functools import partial
 # TODO: remove later
 from transformers import AutoConfig
+# from lmflow.pipeline.auto_pipeline import AutoPipeline
+import evaluate
 import torch.distributed as dist
-
+from transformers import HfArgumentParser
 from lmflow.datasets.dataset import Dataset
 from lmflow.pipeline.base_pipeline import BasePipeline
-from lmflow.models.hf_decoder_model import HFDecoderModel
 from lmflow.utils.data_utils import set_random_seed, batchlize, answer_extraction, load_data
+from lmflow.models.auto_model import AutoModel
+from lmflow.args import ModelArguments, DatasetArguments, AutoArguments
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"  # To avoid warnings about parallelism in tokenizers
 
+# copied from evaluate.py, with small changes.
+pipeline_name = "test_rougel"
+PipelineArguments = AutoArguments.get_pipeline_args_class(pipeline_name)
 
-class Evaluator(BasePipeline):
+parser = HfArgumentParser((ModelArguments, DatasetArguments, PipelineArguments))
+model_args, data_args, pipeline_args = parser.parse_args_into_dataclasses()
+
+with open (pipeline_args.deepspeed, "r") as f:
+    ds_config = json.load(f)
+
+model = AutoModel.get_model(model_args, tune_strategy='none', ds_config=ds_config)
+dataset = Dataset(data_args)
+
+evaluator = AutoPipeline.get_pipeline(
+    pipeline_name=pipeline_name,
+    model_args=model_args,
+    data_args=data_args,
+    pipeline_args=pipeline_args,
+)
+evaluator.evaluate(model=model, dataset=dataset, metric=pipeline_args.metric)
+
+#copied from evaluator.py
+class Test_rougel(BasePipeline):
     """
     Initializes the `Evaluator` class with given arguments.
 
