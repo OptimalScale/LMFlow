b345435cba5fe3dda0d2af5235470d45fa417bf6
Dominic789654 <xliu29@gmu.edu>
2023-06-17 13:51:18 +0000

    add data process script for big dataset

--- scripts/data_preprocess/run_data_preprocess.sh
+++ scripts/data_preprocess/run_data_preprocess.sh
@@ -24,3 +24,8 @@ python scripts/data_preprocess/merge.py \
 python scripts/data_preprocess/concat.py \
     --merge_from_path ./data/example_dataset/train/*.json \
     --output_path ./data/example_dataset/train/train_merge.json \
+
+# For concat_shuffle_split.py: if you simply want to merge multiple files or a directory, use following.
+python scripts/data_preprocess/concat_shuffle_split.py \
+    --merge_from_path ./data/example_dataset/train/*.json \ 
+    --output_path ./data/processed_dataset/ \
