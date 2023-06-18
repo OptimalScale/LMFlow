b5abd56695a75d158f7065cf9c87e90a68a486cb
sYc <songyc9@126.com>
2023-06-17 20:43:29 -0700

    Update rougel_test_case.sh

--- scripts/rougel_test_case.sh
+++ scripts/rougel_test_case.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # 比较self-instruct和LMFlow对相同instructions的ROUGE-L得分是否相同
+# A test case for the ROUGE-L metric used in evaluation.
+# Compares the ROUGE-L scores from self-intruct and LMFlow. Valid if all scores in each pair are the same.
 
 CUDA_VISIBLE_DEVICES=0 \
     deepspeed examples/test.py \
