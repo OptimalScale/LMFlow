942231a2ce8b078cfa0fd01fb1c11d2896bdd193
shizhediao <654745845@qq.com>
2023-07-15 10:38:44 +0800

    change model name

--- service/openai_api.py
+++ service/openai_api.py
@@ -167,8 +167,8 @@ async def predict(query: str, history: List[List[str]], model_id: str):
 
 
 if __name__ == "__main__":
-    tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True)
-    model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).cuda()
+    tokenizer = AutoTokenizer.from_pretrained("LMFlow/Full-Robin-7b-v2", trust_remote_code=True)
+    model = AutoModel.from_pretrained("LMFlow/Full-Robin-7b-v2", trust_remote_code=True).cuda()
     # 多显卡支持，使用下面两行代替上面一行，将num_gpus改为你实际的显卡数量
     # from utils import load_model_on_gpus
     # model = load_model_on_gpus("THUDM/chatglm2-6b", num_gpus=2)
