31d0e1dd23977245bc70c5b446f91b64dc6f8f1c
yaoguany <89233842+yaoguany@users.noreply.github.com>
2023-08-07 21:45:05 +0800

    adjust install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 pip install -e .
 
 gpu_state="$(nvidia-smi --query-gpu=name --format=csv,noheader)"
-if [[ *"A100"* == "${gpu_state}" -o *"A40"* == "${gpu_state}" ]]; then
-  echo "YES!!!!!"
+if [[ "${gpu_state}" == *"A100"* || "${gpu_state}" == *"A40"* ]]; then
   pip install flash-attn==2.0.2
 fi
