0d5cc9d7dd2150068abf44b8b040889c8b34572b
Eric <yizhen.jia96@gmail.com>
2023-09-07 00:14:33 +0800

    SpeculativeInferencer bug fix

--- tests/pipeline/test_spec_inf.py
+++ tests/pipeline/test_spec_inf.py
@@ -3,7 +3,9 @@ from lmflow.args import ModelArguments
 from lmflow.args import DatasetArguments
 from lmflow.models import hf_decoder_model
 from src.lmflow.pipeline.inferencer import SpeculativeInferencer
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 
 model_args = ModelArguments(model_name_or_path='gpt2-large')
 model = hf_decoder_model.HFDecoderModel(model_args)
@@ -15,4 +17,4 @@ data_args = DatasetArguments()
 
 specinf = SpeculativeInferencer(model_args, draft_model_args, data_args, inferencer_args)
 
-specinf.inference(model, draft_model, 'hello, how', gamma=3, max_new_tokens=20)
+specinf.inference(model, draft_model, 'Hello, how are you', gamma=3, max_new_tokens=10)
