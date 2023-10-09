41a2a905673af5734d9d24e876123e1e74a80405
rpan <research4pan@gmail.com>
2023-10-09 21:08:11 +1100

    Add speed test script

---
+++ scripts/run_speedtest.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Please run this script under ${project_id} in project directory of
+#   https://github.com/shizhediao/llm-ft
+#     COMMIT: d5fecf30ba8011067b10cf51fede53a5ab6574e4
+
+# Parses arguments
+model_name_or_path=gpt2
+dataset_path=data/alpaca/train
+output_dir=output_models/finetune
+deepspeed_args="--master_port=11000"
+
+while [[ $# -ge 1 ]]; do
+  key="$1"
+  case ${key} in
+    -m|--model_name_or_path)
+      model_name_or_path="$2"
+      shift
+      ;;
+    -d|--dataset_path)
+      dataset_path="$2"
+      shift
+      ;;
+    -o|--output_model_path)
+      output_dir="$2"
+      shift
+      ;;
+    -b|--per_device_train_batch_size)
+      per_device_train_batch_size=$2
+      shift
+      ;;
+    --deepspeed_args)
+      deepspeed_args="$2"
+      shift
+      ;;
+    *)
+      echo "error: unknown option \"${key}\"" 1>&2
+      exit 1
+  esac
+  shift
+done
+
+# Finetune
+exp_id=finetune
+project_dir=$(cd "$(dirname $0)"/..; pwd)
+log_dir=${project_dir}/log/${exp_id}
+mkdir -p ${output_dir} ${log_dir}
+
+deepspeed ${deepspeed_args} \
+  examples/finetune.py \
+    --model_name_or_path ${model_name_or_path} \
+    --dataset_path ${dataset_path} \
+    --output_dir ${output_dir} --overwrite_output_dir \
+    --num_train_epochs 1 \
+    --learning_rate 2e-5 \
+    --block_size 2048 \
+    --per_device_train_batch_size ${per_device_train_batch_size} \
+    --deepspeed configs/ds_config_zero3.json \
+    --bf16 \
+    --run_name finetune \
+    --validation_split_percentage 0 \
+    --logging_steps 1 \
+    --do_train \
+    --ddp_timeout 72000 \
+    --save_steps 5000 \
+    --dataloader_num_workers 1 \
+    --lr_scheduler_type cosine \
+    --warmup_ratio 0.03 \
+    --gradient_accumulation_steps 1 \
+    --gradient_checkpointing True \
+    | tee ${log_dir}/train.log \
+    2> ${log_dir}/train.err
