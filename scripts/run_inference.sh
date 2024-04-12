6f1936da7a6e13c3c7d2895d46f39e021991bc62
rpan <research4pan@gmail.com>
2024-04-12 17:08:51 +0800

    Add changes of vllm

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -1,21 +1,2 @@
 #!/bin/bash
-# An interactive inference script without context history, i.e. the chatbot
-# won't have conversation memory.
-
-model=gpt2
-lora_args=""
-if [ $# -ge 1 ]; then
-  model=$1
-fi
-if [ $# -ge 2 ]; then
-  lora_args="--lora_model_path $2"
-fi
-
-accelerate launch --config_file configs/accelerator_multigpu_config.yaml \
-  examples/inference.py \
-    --deepspeed configs/ds_config_chatbot.json \
-    --model_name_or_path ${model} \
-    --use_accelerator True \
-    --max_new_tokens 256 \
-    --temperature 1.0 \
-    ${lora_args}
+python3 examples/inference.py --model meta-llama/Llama-2-7b-chat-hf
