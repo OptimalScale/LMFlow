fca0121b2076cbbdb3b8c73de010ba06e0b04f8d
Eric <jia597005217@gmail.com>
2024-04-23 16:10:17 +0000

    Update default dataset and conversation template for lora and qlora shell

--- scripts/run_finetune_with_qlora_save_aggregated_weights.sh
+++ scripts/run_finetune_with_qlora_save_aggregated_weights.sh
@@ -3,8 +3,8 @@
 
 # Parses arguments
 model_name_or_path=meta-llama/Llama-2-13b-hf
-dataset_path=/home/paperspace/LMFlow/alpaca/train
-conversation_template=empty
+dataset_path=data/alpaca/train_conversation
+conversation_template=llama2
 output_dir=output_models/finetune
 deepspeed_args="--master_port=11000"
 
