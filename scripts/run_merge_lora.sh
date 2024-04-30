71ab5a4e0f0145d892ce76e4d3c7b3ac50def3b0
Eric <jia597005217@gmail.com>
2024-04-30 23:06:12 +0800

    Merge lora script update

--- scripts/run_merge_lora.sh
+++ scripts/run_merge_lora.sh
@@ -7,7 +7,7 @@ output_model_path=output_models/merge_lora
 device=cpu
 
 # if gpu
-deepspeed_args="--master_port=11000 --include localhost:2"
+deepspeed_args="--master_port=11000"
 
 while [[ $# -ge 1 ]]; do
   key="$1"
@@ -48,14 +48,8 @@ if [ ${device} == "cpu" ]; then
         --device ${device} \
         --ds_config configs/ds_config_eval.json
 elif [ ${device} == "gpu" ]; then
-    deepspeed ${deepspeed_args} \
-        examples/merge_lora.py \
-        --model_name_or_path ${model_name_or_path} \
-        --lora_model_path ${lora_model_path} \
-        --output_model_path ${output_model_path} \
-        --device ${device} \
-        --ds_config configs/ds_config_zero3_for_eval.json
+    echo "Error: Merging LoRA weights using gpu not supported yet. Please use cpu."
 else
-    echo "error: unknown device \"${device}\"" 1>&2
+    echo "Error: Unknown device \"${device}\"" 1>&2
     exit 1
 fi
