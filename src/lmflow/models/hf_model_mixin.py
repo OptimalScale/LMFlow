777ca4db0938ddef6aade198bb38c13e70867810
Yizhen <yizhen.jia96@gmail.com>
2024-06-12 17:03:09 +0800

    [Usability] HFModelMixin will add pad token if not specified in model cfg

--- src/lmflow/models/hf_model_mixin.py
+++ src/lmflow/models/hf_model_mixin.py
@@ -109,6 +109,9 @@ class HFModelMixin(BaseModel):
         if self.tokenizer.pad_token_id is None:
             self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
             
+        if self.backend_model.config.pad_token_id is None:
+            self.backend_model.config.pad_token_id = self.tokenizer.pad_token_id
+        
 
     def __prepare_tokenizer(
         self,
