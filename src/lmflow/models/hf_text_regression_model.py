f9d99e16bf91e9909f6eb1bd173b3e45223c898a
Eric <jia597005217@gmail.com>
2024-06-15 00:14:49 +0800

    [Usability] Add padding_side to dataset fingerprint

--- src/lmflow/models/hf_text_regression_model.py
+++ src/lmflow/models/hf_text_regression_model.py
@@ -205,6 +205,7 @@ class HFTextRegressionModel(TextRegressionModel, HFModelMixin, Tunable):
                 (
                     raw_datasets.get_fingerprint()
                     + str(self.tokenizer)
+                    + f'###padding_side={self.tokenizer.padding_side}'
                     + ('###conversation_template=' + str(conversation_template) if "conversation" in dataset_type else "")
                     + f'###disable_group_texts={data_args.disable_group_texts}'
                     + f'###block_size={data_args.block_size}'
