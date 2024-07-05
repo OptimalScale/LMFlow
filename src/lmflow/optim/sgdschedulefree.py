7965a04abfd032f535769f7146c907bbce229e92
“TJX” <juanxitian1031@gmail.com>
2024-07-06 00:36:43 +0800

    update

--- src/lmflow/optim/sgdschedulefree.py
+++ src/lmflow/optim/sgdschedulefree.py
@@ -3,6 +3,9 @@
 # 
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import torch
 import torch.optim
 
