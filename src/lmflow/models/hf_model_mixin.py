b14ef5e307b45d04c467e92347844181558d42d4
Yizhen <yizhen.jia96@gmail.com>
2024-07-07 02:57:10 +0800

    [usability] pad token config

--- src/lmflow/models/hf_model_mixin.py
+++ src/lmflow/models/hf_model_mixin.py
@@ -444,6 +444,10 @@ class HFModelMixin(BaseModel):
             self.tokenizer.pad_token = self.tokenizer.eos_token
             
         if not hasattr(self.backend_model.config, "pad_token_id"):
+            logger.warning("pad_token_id not found in model config. Setting pad_token_id to eos_token_id.")
+            self.backend_model.config.pad_token_id = self.backend_model.config.eos_token_id
+        elif self.backend_model.config.pad_token_id is None:
+            logger.warning("pad_token_id is None in model config. Setting pad_token_id to eos_token_id.")
             self.backend_model.config.pad_token_id = self.backend_model.config.eos_token_id
             
 
