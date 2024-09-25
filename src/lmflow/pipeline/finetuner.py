72379a9f734110dc1caed610908706df1beecc7e
Yizhen <yizhen.jia96@gmail.com>
2024-09-25 12:33:38 +0800

    [bug fix] temporarily disable saving training args when lisa

--- src/lmflow/pipeline/finetuner.py
+++ src/lmflow/pipeline/finetuner.py
@@ -490,6 +490,54 @@ class Finetuner(BaseTuner):
                 self.create_optimizer()
                 self.optimizer = self.accelerator.prepare(self.optimizer)
                 
+            def _save(self, output_dir: Optional[str] = None, state_dict=None):
+                import torch
+                from transformers.utils import (
+                    is_safetensors_available, 
+                    is_peft_available, 
+                    SAFE_WEIGHTS_NAME, 
+                    WEIGHTS_NAME
+                )
+                if is_safetensors_available():
+                    import safetensors.torch
+                if is_peft_available():
+                    from peft import PeftModel
+                
+                # If we are executing this function, we are the process zero, so we don't check for that.
+                output_dir = output_dir if output_dir is not None else self.args.output_dir
+                os.makedirs(output_dir, exist_ok=True)
+                logger.info(f"Saving model checkpoint to {output_dir}")
+
+                supported_classes = (PreTrainedModel,) if not is_peft_available() else (PreTrainedModel, PeftModel)
+                # Save a trained model and configuration using `save_pretrained()`.
+                # They can then be reloaded using `from_pretrained()`
+                if not isinstance(self.model, supported_classes):
+                    if state_dict is None:
+                        state_dict = self.model.state_dict()
+
+                    if isinstance(self.accelerator.unwrap_model(self.model), supported_classes):
+                        self.accelerator.unwrap_model(self.model).save_pretrained(
+                            output_dir, state_dict=state_dict, safe_serialization=self.args.save_safetensors
+                        )
+                    else:
+                        logger.info("Trainer.model is not a `PreTrainedModel`, only saving its state dict.")
+                        if self.args.save_safetensors:
+                            safetensors.torch.save_file(
+                                state_dict, os.path.join(output_dir, SAFE_WEIGHTS_NAME), metadata={"format": "pt"}
+                            )
+                        else:
+                            torch.save(state_dict, os.path.join(output_dir, WEIGHTS_NAME))
+                else:
+                    self.model.save_pretrained(
+                        output_dir, state_dict=state_dict, safe_serialization=self.args.save_safetensors
+                    )
+
+                if self.tokenizer is not None:
+                    self.tokenizer.save_pretrained(output_dir)
+
+                # Good practice: save your training arguments together with the trained model
+                # torch.save(self.args, os.path.join(output_dir, TRAINING_ARGS_NAME))
+                
         return LISATrainer
 
     def tune(self,
